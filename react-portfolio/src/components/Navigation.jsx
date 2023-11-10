2cdc21f49bb6cb9ca3561d9e1c299e14d8ebd66e
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-11-10 13:25:39 -0700

    added logic to project component

--- react-portfolio/src/components/Navigation.jsx
+++ react-portfolio/src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import '../styles/Navigation.css';
 function Navigation() {
     return (
         <nav className="navbar">
-            <a href="#bio">Biography</a>
+            <a href="#aboutme">About Me</a>
             <a href="#portfolio">Portfolio</a>
             <a href="#contact">Contact Information</a>
             <a href="#resume">Resume</a>
